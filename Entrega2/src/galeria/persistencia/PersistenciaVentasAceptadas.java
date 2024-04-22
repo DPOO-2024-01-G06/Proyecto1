7af1f968586bb6127522ffc873945c609cf29ad3
Tatiana020 <141865396+Tatiana020@users.noreply.github.com>
2024-04-21 23:40:09 -0500

    cambio PersistenciaVentasAceptadas.java

--- Entrega2/src/galeria/persistencia/PersistenciaVentasAceptadas.java
+++ Entrega2/src/galeria/persistencia/PersistenciaVentasAceptadas.java
@@ -1,20 +1,42 @@
 package persistencia;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import galeria.structurer_inventario.Venta;
 
-public class PersistenciaVentasAceptadas extends PersistenciaCentral<Map<Integer, Venta>> {
-    private static final String ARCHIVO_VENTAS_ACEPTADAS = "ventas_aceptadas.dat";
+public class PersistenciaVentasAceptadas {
 
-    @Override
-    public void guardar(Map<Integer, Venta> ventasAceptadas) {
-        super.guardar(ventasAceptadas);
-    }
+	private String archivoSubastasPasadas;
 
-    @Override
-    public Map<Integer, Venta> cargar() {
-        return super.cargar();
-    }
+	public PersistenciaVentasAceptadas(String archivoVentasAceptadas) {
+		this.archivoVentasAceptadas = archivoVentasAceptadas;
+	}
+
+	 public static <V> void guardarVentasAceptadas(Map<?, V> mapa, String archivoVentasAceptadas) {
+	     try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivoVentasAceptadas))) {
+	            for (V valor : mapa.values()) {
+	                writer.write(valor.toString());
+	                writer.newLine();
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	 
+	 public static Map<Integer, String> cargarMapa(String archivoVentasAceptadas) {
+	        Map<Integer, String> mapa = new HashMap<>();
+	        try (BufferedReader reader = new BufferedReader(new FileReader(archivoVentasAceptadas))) {
+	            String linea;
+	            int contador = 0;
+	            while ((linea = reader.readLine()) != null) {
+	                mapa.put(contador++, linea);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        return mapa;
+	    }
 }
